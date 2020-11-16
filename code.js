8115d43e1b9b1a16501600bb85e0cb08ccaf0bc9
Denys <44090130+denyskalinichenko@users.noreply.github.com>
2020-11-16 20:19:57 +0100

    Add files via upload

---
+++ code.js
@@ -0,0 +1,14 @@
+const btn = document.querySelector('button');
+const div = document.querySelector('div');
+
+const names = ['Ola', 'Denis', 'Kristina', 'Ivan']
+// "Math.random()" - da nam znaczenia od o do n.999
+// "Math.floor()" - zaokrągla nam znaczenia
+
+
+const nameGenerator = () => {
+    const index = Math.floor(Math.random() * names.length);
+    div.textContent = `Najlepsze imię dla dziecka będzie: ${names[index]}`
+}
+
+btn.addEventListener('click', nameGenerator);
